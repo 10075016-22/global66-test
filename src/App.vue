fab688ce47b25f166ba1c3a4c7718bf67e3586fe
Thiago Lopez <thiago.lopez@seif.com.co>
2025-02-22 23:14:52 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app-content flex justify-center items-center">
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+* {
+  font-family: 'Lato', serif !important;
+}
+.app-content {
+  background-color: #F9F9F9 !important;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <div>
+            <div class="flex flex-col items-center justify-center min-h-screen text-center">
+                <img :src="pikachu" alt="Pikachu" class="mb-15" />
+                <h1 class="text-2xl text-gray-800 font-bold">Welcome to Pokédex</h1>
+                <p class="text-lg mt-6 max-w-xl typography-paragraph">
+                    The digital encyclopedia created by Professor Oak is an invaluable tool to Trainers in the Pokémon
+                    world.
+                </p>
+                <button class="mt-13 px-4 py-2 bg-red-500 text-white text-lg font-semibold rounded-full shadow-md hover:bg-red-600 transition" @click="onGetStarted">
+                    Get started
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+import pikachu from "@/assets/pikachu.svg";
+
+const router = useRouter()
+const onGetStarted = () => {
+    router.replace({name: 'dashboard'})
+}
+
+</script>
+<style lang="scss" scoped>
+.typography-paragraph {
+    line-height: 1.688rem;
+    color: #5E5E5E;
+    font-weight: 500 !important;
+}
+</style>
